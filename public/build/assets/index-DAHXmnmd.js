9da80a7bdf2640e3500dcb39f145ed21d95df120
Hisukawa <hisukawataiki123@gmail.com>
2025-10-09 00:49:23 +0800

    fourth commit

---
+++ public/build/assets/index-DAHXmnmd.js
@@ -0,0 +1,1 @@
+import{g as r,b as t}from"./app-CAI_Xi7A.js";var a=t();const e=r(a);export{e as R,a as r};
--- public/build/assets/skeleton-BJ7hj38Q.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-DluTzGHb.js";import{c as o}from"./button-Dh7fzRxe.js";function a({className:r,...e}){return m.jsx("div",{className:o("animate-pulse rounded-md bg-primary/10",r),...e})}export{a as S};
