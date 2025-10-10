7fd61a38e0aabbcfabe4bd0b9af5f6cd7e27f157
Hisukawa <hisukawataiki123@gmail.com>
2025-10-11 06:00:58 +0800

    ken push

---
+++ public/build/assets/index-D20yT8t5.js
@@ -0,0 +1,1 @@
+import{g as r,b as t}from"./app-CxQIk-Ug.js";var a=t();const e=r(a);export{e as R,a as r};
---
+++ public/build/assets/useAppUrl-CjqOY4Q6.js
@@ -0,0 +1,1 @@
+import{r as t}from"./app-CxQIk-Ug.js";const s=()=>{const r="https://ilagan.bims-ph.online/".replace(/\/$/,"");return t.useMemo(()=>r,[])};export{s as u};
